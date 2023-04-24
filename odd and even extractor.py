f570869a8da410bb2c8e369ffd63331c0dc68a30
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 19:11:04 +0800

    create and open txt files

--- odd and even extractor.py
+++ odd and even extractor.py
@@ -3,11 +3,13 @@
 # This program reads a text file that contains a set of numbers. 
 # Then, the even and odd numbers will be extracted from the text file and will be transferred to separate files.
 
-
 # open numbers.txt (read), even.txt(write), odd.txt(write)
-# read numbers.txt line by line
-# each line
-# if even,
-	# write to even.txt
-# if odd,
-	# write to odd.txt
+with open("numbers.txt", 'r') as input_file, open("even.txt", 'w') as _file2,  open("odd.txt", 'w') as my_file3:
+    """
+    # read numbers.txt line by line
+    #  each line
+    #  if even,
+        # write to even.txt
+    #  if odd,
+        #  write to odd.txt
+    """
