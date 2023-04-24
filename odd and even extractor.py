05e88c9eaa64a9ee145a34cff2c582864bc0b022
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 19:34:15 +0800

    extracting odd numbers

--- odd and even extractor.py
+++ odd and even extractor.py
@@ -3,6 +3,7 @@
 # This program reads a text file that contains a set of numbers. 
 # Then, the even and odd numbers will be extracted from the text file and will be transferred to separate files.
 
+
 # open numbers.txt (read), even.txt(write), odd.txt(write)
 with open("numbers.txt", 'r') as input_file, open("even.txt", 'w') as output_even,  open("odd.txt", 'w') as output_odd:
     
@@ -12,13 +13,15 @@ with open("numbers.txt", 'r') as input_file, open("even.txt", 'w') as output_eve
         #  convert each line from numbers.txt into integer
         extracted_number = int(line)  
 
-        #  if the extracted no. is even
+        #  if the extracted number is even
         if extracted_number % 2 == 0:  
             
             # write to even.txt
             output_even.write(str(extracted_number)+ "\n")
             
+        #  if the extracted number is odd
+        if extracted_number % 2 == 1: 
+
             """
-        #  if odd,
             #  write to odd.txt
-            """ 
+            """
