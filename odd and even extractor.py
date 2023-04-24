a08c51c4d762578a33a58f75a59b8908d7a547e8
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 18:12:39 +0800

    pseudocode

---
+++ odd and even extractor.py
@@ -0,0 +1,13 @@
+# Bernido, Charles David P. | BSCPE 1-5
+# Object Oriented Programming | Assignment 4 - Problem 1
+# This program reads a text file that contains a set of numbers. 
+# Then, the even and odd numbers will be extracted from the text file and will be transferred to separate files.
+
+
+# open numbers.txt (read), even.txt(write), odd.txt(write)
+# read numbers.txt line by line
+# each line
+# if even,
+	# write to even.txt
+# if odd,
+	# write to odd.txt
