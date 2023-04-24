22fd80e6e9d6b4480c553467d95010ef0846d8d0
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-24 19:18:39 +0800

    read numbers,txt line by line

--- odd and even extractor.py
+++ odd and even extractor.py
@@ -4,12 +4,15 @@
 # Then, the even and odd numbers will be extracted from the text file and will be transferred to separate files.
 
 # open numbers.txt (read), even.txt(write), odd.txt(write)
-with open("numbers.txt", 'r') as input_file, open("even.txt", 'w') as _file2,  open("odd.txt", 'w') as my_file3:
-    """
+with open("numbers.txt", 'r') as input_file, open("even.txt", 'w') as output_even,  open("odd.txt", 'w') as output_odd:
+    
     # read numbers.txt line by line
-    #  each line
-    #  if even,
-        # write to even.txt
-    #  if odd,
-        #  write to odd.txt
-    """
+    for line in input_file:
+        
+        """
+        #  each line
+        #  if even,
+            # write to even.txt
+        #  if odd,
+            #  write to odd.txt
+        """
