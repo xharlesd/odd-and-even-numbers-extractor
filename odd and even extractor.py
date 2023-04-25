50cf916f330ab9aacdeadb63c037d7f6d7b9bdd6
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-25 17:43:41 +0800

    add break sa else

--- odd and even extractor.py
+++ odd and even extractor.py
@@ -14,13 +14,13 @@ def user_input():
             if user_input.isnumeric():
 
                 # user input will be written to numbers.txt
-                input_file1.write(user_input)
-
+                input_file1.write(str(user_input) + '\n')
+            
             # if user input is not a number
             else: 
                 print("Do you want to try again? ")
-                exit()
-                
+                break
+
 def main():
 # open numbers.txt (read), even.txt(write), odd.txt(write)
     with open("numbers.txt", 'r') as input_file2, open("even.txt", 'w+') as output_even,  open("odd.txt", 'w') as output_odd:
